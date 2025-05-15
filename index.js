a8c2cc7087a8eb98e1e3c7aa328f6f93d82209e9
Taichi_Sato_Matsue_Kosen <114867136+aruteko@users.noreply.github.com>
2025-05-15 05:30:59 +0000

    second_ex

--- index.js
+++ index.js
@@ -2,13 +2,43 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('ねむてぇ!')
-})
+// app.get('/', (req, res) => {
+//   res.send('ねむてぇ!')
+// })
 
-app.get('/users/:id', function (req, res) {
-  res.send('User ID: ' + req.params.id)
-})
+// app.get('/users/:id', function (req, res) {
+//   res.send('User ID: ' + req.params.id)
+// })
+
+// GETリクエスト
+app.get('/', (req, res) => {
+  res.send({
+    msg:'GET request'
+  });
+});
+
+// POSTリクエスト
+app.post('/', (req, res) => {
+  res.send({
+    msg:'POST request'
+  });
+});
+
+// PUTリクエスト
+app.put('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'PUT request'
+  });
+});
+
+// DELETEリクエスト
+app.delete('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'DELETE request'
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
